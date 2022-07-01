89180f93bfb0b82dd353132cfdb12f4c12c7e6ff
BiGG D4MSsSs <damian.rekik@gmail.com>
2022-07-01 23:43:18 +0200

    lettre animation

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,9 +1,22 @@
-let a = document.getElementsByClassName("lettreAnimer")
+let spanClass = document.querySelectorAll("#Presentation span")
+for (const spans of spanClass) {
+  if (!spans.id.includes("prenomPresentation")) {
+  spans.classList.add("animation")
+  }
 
+}
+let animer = document.getElementsByClassName("animation");
 
-for (const anime of a) {
-
-anime.addEventListener("click", function () {
-anime.classList.add("animate__animated animate__jello");
-})
+for (const anime of animer) {
+  anime.classList.add("animate__animated");
+  anime.addEventListener("mouseenter", function () {
+    anime.classList.add("animate__jello");
+    anime.style.color = "aqua";
+  });
+  anime.addEventListener("animationend", function () {
+    anime.classList.remove("animate__jello")
+  })
+  anime.addEventListener("mouseleave", function () {
+    anime.style.color=""
+  })
 }
